5713196f018eccccf5536fcc65c46877d91a4b3d
exsanik <nexsanik@gmail.com>
2019-08-10 14:44:43 +0300

    Updates

---
+++ precache-manifest.484c37203a8530eb7825e1cc17645adc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15789c94e1bd9311726debce82d30f5",
+    "url": "/react-bookshop/index.html"
+  },
+  {
+    "revision": "0516797de8ba04d91661",
+    "url": "/react-bookshop/static/css/main.29c0cfc5.chunk.css"
+  },
+  {
+    "revision": "e1df879db4609e3d77a5",
+    "url": "/react-bookshop/static/js/2.1bb3fb32.chunk.js"
+  },
+  {
+    "revision": "0516797de8ba04d91661",
+    "url": "/react-bookshop/static/js/main.094e30a5.chunk.js"
+  },
+  {
+    "revision": "da3bca46b887855dd4ed",
+    "url": "/react-bookshop/static/js/runtime~main.a44e2d52.js"
+  }
+]);
